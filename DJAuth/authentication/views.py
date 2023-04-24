02550cd8e649b837657cc351699523e8af7d747e
Ifiok Ambrose <61599279+Ambrose280@users.noreply.github.com>
2023-04-24 14:24:32 +0100

    first commit

---
+++ DJAuth/authentication/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+# Create your views here.
+
+def main(request):
+    return HttpResponse("Successfully logged in!")
+
